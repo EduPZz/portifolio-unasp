cb9a5c82b9407815dc922193e77fd3da75de3f85
EduPZz <eduardo.pozzogood@hotmail.com>
2024-11-06 00:46:37 +0000

    First commit

---
+++ info.js
@@ -0,0 +1,8 @@
+const coder = {
+    name: 'Eduardo',
+    company: 'Bradial Digital Solutions',
+    location: 'Hortolândia, SP',
+    repositories: 30,
+    skills: ['React', 'NextJS', 'Redux', 'Express', 'NestJS', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
+    timeZone: 'UTC +3'
+}
